9e30c03bbfff2eb642e8d5b66565021396c98aaf
Reuben Cummings <reubano@gmail.com>
2013-05-07 14:11:38 +0300

    Switch to express server

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-var statik = require('statik');
-statik(process.env.PORT || 1337);
+var express = require('express');
+var app = express();
+var oneDay = 86400000;
+
+app.use(express.compress());
+app.use(express.static(__dirname + '/public', {maxAge: oneDay}));
+app.listen(process.env.PORT || 3000);
