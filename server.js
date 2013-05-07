8fd40cc607938af0fe8b9f466dadc31af980e2ed
Reuben Cummings <reubano@gmail.com>
2013-05-07 14:46:32 +0300

    Add console log

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var app = express();
-var oneDay = 86400000;
+var express = require('express'),
+	app = express(),
+	oneDay = 86400000,
+	port = process.env.PORT || 3000;
 
 app.use(express.compress());
 app.use(express.static(__dirname + '/public', {maxAge: oneDay}));
-app.listen(process.env.PORT || 3000);
+app.listen(port, function() {console.log("listening on " + port); });
