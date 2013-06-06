2bd061b0fffa882c43e56ed64ad2fe964c63dc48
Reuben Cummings <reubano@gmail.com>
2013-06-06 19:16:49 +0300

    Move charting script to sep file

---
+++ app/assets/gen-chart.js
@@ -0,0 +1,58 @@
+var chartRange, maxTime, minTime, tickInterval;
+
+minTime = 7.5;
+maxTime = 18.5;
+chartRange = [minTime * 60, maxTime * 60];
+tickInterval = [];
+
+formatMinutes = function(d) {
+	var time = d3.time.format("%I:%M %p")(new Date(2013, 0, 1, 0, d));
+	return time.substr(0,1) == '0' ? time.substr(1) : time;
+};
+
+makeChart = function(data, div) {
+	console.log('starting...');
+	var id = div.id
+	console.log('making chart ' + id);
+
+	$(document).ready(function(){
+		selection = '#' + id +'.view .chart svg';
+
+		chart = nv.models.multiBarHorizontalChart()
+			.x(function(d) {return d.label})
+			.y(function(d) {return d.value})
+			.forceY(chartRange)
+			.yDomain(chartRange)
+			.margin({top: 0, right: 110, bottom: 30, left: 80})
+			//.showValues(true)
+			//.tooltips(false)
+			.stacked(true)
+			.showLegend(false)
+			.barColor([d3.rgb('steelblue')])
+			.showControls(false)
+			;
+
+		for (var i = 0; i < maxTime - 1; i++) {
+			tickInterval[i] = (minTime + i + 1) * 60;
+		}
+
+		chart.yAxis
+			.tickValues(tickInterval)
+			.tickFormat(formatMinutes)
+
+		chart.multibar.yScale().clamp(true);
+
+		d3.select(selection)
+			.datum(data)
+			.transition().duration(100)
+			.call(chart);
+
+		// nv.utils.windowResize(chart.update);
+
+		chart.dispatch.on('stateChange', function(e) {
+			nv.log('New State:', JSON.stringify(e));
+		});
+
+		nv.addGraph(chart);
+	});
+};
--- app/views/graph-view.coffee
+++ app/views/graph-view.coffee
@@ -19,11 +19,13 @@ module.exports = class GraphView extends View
 		attr = 'prev_work'
 		chart_data = @model.get attr + '_chart_data'
 		name = @model.get 'first_name'
+		id = @model.get 'id'
 
 		if chart_data and name
 			console.log @model.get('id') + ' has ' + attr + '_chart_data'
 			@model.setChartData(attr)
-			# @$('#draw').html "nv.addGraph({{{chart}}})"
+			script = "<script>makeChart(#{chart_data}, #{id});</script>"
+			@$('#draw').html script
 		else
 			console.log @model.get('id') + ' has no ' + attr + '_chart_data' +
 				' or no name'
