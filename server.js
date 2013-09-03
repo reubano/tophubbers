85829cc9321bd487a6f52a3601c00f9442d8a994
Reuben Cummings <reubano@gmail.com>
2013-09-03 22:58:49 +0300

    Add minilog

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express'),
 	winston = require('winston'),
+	log = require('minilog')('app'),
 	app = express(),
 	oneDay = 86400000,
 	port = process.env.PORT || 3000;
+
+require('minilog').enable();
 
 app.use(express.compress());
 app.use(express.static(__dirname + '/public', {maxAge: oneDay}));
