ca52b7959053b2634c6f4cc2fef754a349e3567f
Reuben Cummings <reubano@gmail.com>
2013-09-05 09:13:42 +0300

    Implement winston on server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express'),
 	winston = require('winston'),
-	log = require('minilog')('app'),
 	app = express(),
 	oneDay = 86400000,
 	port = process.env.PORT || 3333;
 
-require('minilog').enable();
+// enable web server logging piped through winston
+var winstonStream = {
+    write: function(message, encoding){
+        winston.info(message);
+    }
+};
 
+app.use(express.logger({stream:winstonStream}));
 app.use(express.compress());
 app.use(express.static(__dirname + '/public', {maxAge: oneDay}));
 app.use(function(req, res) {
